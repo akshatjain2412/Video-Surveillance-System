68c4b25079e54dcf563961e5d7f5d179fde21dcf
Akshat Jain <Akshatjain241203@gamil.com>
2024-07-26 21:32:11 +0530

    initial commit

---
+++ test.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:4ab63f7748b88999422def330ba550107627ce5cf68697c01925f46e2c662859
+size 2106
---
+++ train.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:8ba053f039df09214c66487ee508e43579594f3a62e53ab9d2e447357d361b8d
+size 2859
